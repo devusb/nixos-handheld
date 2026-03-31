1f0d6833f9ccdac2b1e4609d25faecc9791f6480
Morgan Helton <mhelton@gmail.com>
2026-03-30 22:20:24 -0500

    feat: add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, treefmt-nix, systems }:
     let
       system = "aarch64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
     {
       overlays.default = import ./overlay.nix;
@@ -30,5 +34,7 @@
         r36h-image = self.nixosConfigurations.r36h.config.system.build.sdImage;
         linux-rk3326 = pkgs.callPackage ./pkgs/kernel-rk3326 { };
       };
+
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
     };
 }
