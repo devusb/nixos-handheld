dd0788c9d956af5996669b31583c32db72faacc0
Morgan Helton <mhelton@gmail.com>
2026-08-16 21:43:55 -0500

    fix(pkgs/balatro): src -> overriddenSrc

--- pkgs/balatro/default.nix
+++ pkgs/balatro/default.nix
@@ -10,7 +10,7 @@ let
   };
 in
 balatro.override {
-  src = requireFile {
+  overriddenSrc = requireFile {
     name = "Balatro.exe";
     url = "https://store.steampowered.com/app/2379780/Balatro/";
     hash = "sha256-DXX+FkrM8zEnNNSzesmHiN0V8Ljk+buLf5DE5Z3pP0c=";
